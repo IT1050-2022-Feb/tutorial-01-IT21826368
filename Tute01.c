d63c15da9522113ac2be53bf08721a0495d5d07f
IT21826368 <110513504+IT21826368@users.noreply.github.com>
2022-08-03 04:36:11 +0000

    what do i change.

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,17 @@
 #include <stdio.h>
 
 int main() {
+  int sub1,sub2;
+  float avg;
+
+  printf("Enter sub1 :\n");
+  scanf("%d",&sub1);
+  printf("Enter sub2 :\n");
+  scanf("%d",&sub2);
   
+  avg =(sub1+sub2)/2.00;
+  
+  printf("Average is %.2f",avg);
   return 0;
 }
 
